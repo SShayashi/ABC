44c26958a0954331209561a7b1475597a6aff9ba
hayashi <sshayashi0208@gmail.com>
2018-01-26 05:02:44 +0900

    😭 WA

---
+++ abc084/c.py
@@ -0,0 +1,18 @@
+
+def main():
+    N = int(input())
+
+    x = [[int(a) for a in input().split()] for i in range(N - 1)]
+
+    for i in range(N):
+        time = 0
+        for a in range(i, N-1):
+            C = x[a][0]
+            S = x[a][1]
+            F = x[a][2]
+            time = max(time, S)
+            time += time % F
+            time += C
+        print(time)
+
+main()
