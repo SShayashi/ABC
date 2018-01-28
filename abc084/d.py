67496c0295f04d0afd507c57d21d88a91e16ad36
hayashi <sshayashi0208@gmail.com>
2018-01-28 15:12:21 +0900

    😀 AC

--- abc084/d.py
+++ abc084/d.py
@@ -5,6 +5,30 @@ def is_prime(q):
     return pow(2, q - 1, q) == 1
 
 
+def get_prime_array(N):
+    f = [0] * (N + 1)
+    for i in range(2, N + 1):
+        if not f[i]:
+            for j in range(i + i, N + 1, i):
+                f[j] = 1
+    return f
+
+
+def ans():
+    Q = int(input())
+    f = get_prime_array(1000001)
+    c = [0] * 1000002
+    for i in range(3, 1000001, 2):
+        if not f[i] and not f[(i + 1) // 2]:
+            c[i] += 1
+
+    for i in range(3, 1000000):
+        c[i] = c[i] + c[i - 1]
+
+    for _ in range(Q):
+        l, r = map(int, input().split())
+        print(c[r] - c[l-1])
+
 def main():
     Q = int(input())
     dp = [-1 for _ in range(1000000)]
@@ -17,7 +41,7 @@ def main():
                     cnt += 1
                     continue
 
-            if is_prime(x) and is_prime((x+1)//2):
+            if is_prime(x) and is_prime((x + 1) // 2):
                 dp[x] = 1
                 cnt += 1
             else:
@@ -25,4 +49,5 @@ def main():
 
         print(cnt)
 
-main()
+ans()
+# main()
