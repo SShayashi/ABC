3f18786d92b0d7781a25300829f5105bfbfbf56c
hayashi <sshayashi0208@gmail.com>
2018-02-20 05:15:14 +0900

    🤑 AC

--- abc088/c.py
+++ abc088/c.py
@@ -1,3 +1,29 @@
+def ans():
+    C = []
+    C.append([])
+    C.append(list(map(int, ('0 ' + input()).split())))
+    C.append(list(map(int, ('0 ' + input()).split())))
+    C.append(list(map(int, ('0 ' + input()).split())))
+
+    A = [0] * 4
+    B = [0] * 4
+    A[1] = 0
+
+    for i in range(1,4):
+        B[i] = C[1][i] - A[1]
+
+    for i in range(2, 4):
+        A[i] = C[i][1] - B[1]
+
+    for i in range(1,4):
+        for j in range(1, 4):
+            if C[i][j] == (A[i] + B[j]):
+                pass
+            else:
+                return print('No')
+    return print('Yes')
+
+
 def main():
     def new_A(a1):
         A[1] = a1
@@ -30,4 +56,5 @@ def main():
         if check(A):
             return print('Yes')
 
-main()
+# main()
+ans()
