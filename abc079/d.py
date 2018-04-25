40757fd3fba6127075c33e7219eaf0652b94a021
hayashi <sshayashi0208@gmail.com>
2018-04-25 17:14:06 +0900

    😜AC

--- abc079/d.py
+++ abc079/d.py
@@ -1,37 +1,32 @@
-MAX = 999999
+MAX = 99999999
 
-def fromto(i, j):
 
-    pass
+def solve(dp):
+    for k in range(10):
+        for i in range(10):
+            for j in range(10):
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
 
-def solve(dp, A):
-    nowcost = MAX
-    for i in range(10):
-        for j in range(10):
-            A[i][j]
-            pass
-        pass
     return dp
 
 
 def m():
     H, W = map(int, input().split())
-    c = [[map(int, input().split()) for _ in range(10)] for _ in range(10)]
-    A = [[map(int, input().split()) for _ in range(W)] for _ in range(H)]
+    c = [list(map(int, input().split())) for _ in range(10)]
+    A = [list(map(int, input().split())) for _ in range(H)]
     ans = [0 for _ in range(10)]
-    dp = [[MAX] * 10 for _ in range(10)]
-    for p in range(10):
-        dp[p][p] = 0
-        dp[p][1] = A[p][1]
-
-    dp = solve(dp, A)
+    # dp = [[MAX] * 10 for _ in range(10)]
+    c = solve(c)
 
     for a in A:
         for i in a:
             if i != -1:
                 ans[i] += 1
 
-    pass
+    cost = 0
+    for i in range(10):
+        cost += c[i][1] * ans[i]
+    return cost
 
 
-m()
+print(m())
