fbb3bf44e1281e0108ee34fe10da08e949a13bf0
hayashi <sshayashi0208@gmail.com>
2018-04-27 00:22:32 +0900

    👯AC

--- abc078/d.py
+++ abc078/d.py
@@ -1,7 +1,9 @@
 def m():
     N, Z, W = map(int, input().split())
-    a = list(map(int , input().split()))
-    diff = [0]*N
+    a = list(map(int, input().split()))
+    if N == 1:
+        return abs(a[0] - W)
+    return max(abs(a[N - 1] - a[N - 2]), abs(a[N - 1] - W))
 
 
 print(m())
