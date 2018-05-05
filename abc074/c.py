f0352445f07c34608c4e1a00bf203fa2c81fe4e2
hayashi <sshayashi0208@gmail.com>
2018-05-05 23:55:11 +0900

    WIP

--- abc074/c.py
+++ abc074/c.py
@@ -1,7 +1,13 @@
 def m():
     A, B, C, D, E, F = map(int, input().split())
 
-    pass
+    ans = 0
+    for i in range(1, F):
+        for k in range(1, F):
+            x = A * 100 * i + B * 100 * k
+            y = C * i + D * k
+            if x / 100 >= E * y:
+                ans = max((100 * x) / (x + y), ans)
+    return ans
 
-
-m()
+print(m())
