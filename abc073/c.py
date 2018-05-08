13159ca1b0e8b2773026ad36e3a89c37ea77f2b9
hayashi <sshayashi0208@gmail.com>
2018-05-08 21:03:34 +0900

    😤AC

--- abc073/c.py
+++ abc073/c.py
@@ -1,8 +1,21 @@
 def m():
     N = int(input())
     A = [int(input()) for _ in range(N)]
-    ans = []
-    for a in A: ans.remove(a) if a in ans else ans.append(a)
-    return len(ans)
+    A.sort()
+    tmp = -1
+    cnt = 0
+    ans = 0
+    for i in range(N):
+        if tmp == A[i]:
+            cnt +=1
+        else:
+            if cnt % 2 == 0: pass
+            else: ans +=1
+            tmp = A[i]
+            cnt = 1
+
+    if cnt % 2 == 0: pass
+    else: ans += 1
+    return ans
 
 print(m())
