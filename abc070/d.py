6eb61d14cc105c757ce481206651d375b13c385c
hayashi <sshayashi0208@gmail.com>
2018-05-17 12:03:24 +0900

    WIP

--- abc070/d.py
+++ abc070/d.py
@@ -34,4 +34,26 @@ def m():
         ds = dijkstra(ds, x)
         print(ds[x][k] + ds[k][y])
 
-m()
+def ans():
+    def dfs(v, p, d):
+        depth[v] = d
+        for e, cost in tree[v]:
+            if e == p:
+                continue
+            dfs(e, v, d + cost)
+    n = int(input())
+    tree = [[] for _ in range(n+1)]
+    depth = [0] * 100001
+    for _ in range(1,n):
+        a, b, c = map(int, input().split())
+        tree[a].append((b,c))
+        tree[b].append((a,c))
+
+    q, k = map(int, input().split())
+    dfs(k, -1, 0)
+    for _ in range(q):
+        x, y = map(int, input().split())
+        print(depth[x] + depth[y])
+
+ans()
+# m()
