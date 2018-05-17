6b87ce731dd3a6634d47329ff70cbde8df3b69da
hayashi <sshayashi0208@gmail.com>
2018-05-17 09:22:08 +0900

    😆AC

--- abc070/c.py
+++ abc070/c.py
@@ -1,21 +1,18 @@
+def gcd(a,b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def lcm(a,b):
+    return a * b // gcd(a,b)
+
 def m():
     N = int(input())
     T = [int(input()) for _ in range(N)]
-    ans = []
-    T.sort()
-    for i in range(len(T)):
-        tmp = T[i]
-        flag = 0
-        for k in range(i+1, len(T)):
-            if T[k] % tmp == 0:
-                flag = 1
-                break
-        if flag == 0:
-            ans.append(tmp)
-    ans = set(ans)
-    a = 1
-    for j in ans:
-        a *= j
-    return a
+    ans = T[0]
+    for i in range(1,N):
+        ans = lcm(ans,T[i])
+    return ans
 
 print(m())
