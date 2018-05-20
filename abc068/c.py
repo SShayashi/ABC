28a751be31d4dbd353eacdcf13849902fa962777
hayashi <sshayashi0208@gmail.com>
2018-05-20 16:34:41 +0900

    😆AC

--- abc068/c.py
+++ abc068/c.py
@@ -1,5 +1,21 @@
-def m():
-    pass
+def main():
+    n, m = map(int, input().split())
+    a, b = [], []
+    from_1 = set()
+    to_n = set()
+    for _ in range(m):
+        x, y = map(int, input().split())
+        if x == 1:
+            from_1.add(y)
+            continue
+        if y == n:
+            to_n.add(x)
+            continue
+    ans = from_1 & to_n
+    if ans:
+        return "POSSIBLE"
+
+    return 'IMPOSSIBLE'
 
 
-print(m())
+print(main())
