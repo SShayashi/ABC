3fa90ee1e8df349a6640cf80e7b4c3ad70e57d60
hayashi <sshayashi0208@gmail.com>
2018-05-24 23:43:42 +0900

    😭Give up

--- abc065/d.py
+++ abc065/d.py
@@ -16,5 +16,44 @@ def main():
     l = list(zip(X,Y))
 
 
+def someone_ans():
+    n = int(input())
+    towns = []
+    for i in range(n):
+        x, y = map(int, input().split())
+        towns.append((i,x,y)) # 要素の中に番号いれておくべきだった
+
+
+    adj = [[] for _ in range(n)]
+    for xy in [1,2]:
+        towns.sort(key=lambda t: t[xy])
+        for i in range(n - 1):
+            t1 = towns[i][0] #街の識別番号とってる
+            t2 = towns[i +1][0]
+            cost = towns[i + i][xy] - towns[i][xy]
+            #　隣接リストを作っている
+            adj[t1].append((t2, cost))
+            adj[t2].append((t1, cost))
+
+    #ここからプリム法
+    import heapq
+    pq = []
+    heapq.heappush(pq, (0,0))
+    done = set()
+    ans = 0
+    while pq:
+        cost, v_now = heapq.heappop(pq)
+        if v_now in done:
+            continue
+
+        ans += cost
+        done.add(v_now)
+        if len(done) == n:
+            break
+
+        for v, c in adj[v_now]:
+            if v not in done:
+                heapq.heappush(pq, (c, v))
+
 
 print(main())
