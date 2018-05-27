4dd929b7ba3de003839f488825d45ea9c648e1fd
hayashi <sshayashi0208@gmail.com>
2018-05-27 21:24:51 +0900

    😭WA

---
+++ abc098/d.py
@@ -0,0 +1,12 @@
+
+def m():
+    n = int(input())
+    a = list(map(int, input().split()))
+    for l in range(n):
+        for r in range(l,n):
+            pass
+        pass
+
+    pass
+
+print(m())
