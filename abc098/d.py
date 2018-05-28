7f37a09d390365fd14e0da35c28f4670d2f42a95
hayashi <sshayashi0208@gmail.com>
2018-05-28 22:20:32 +0900

    😭Give up

--- abc098/d.py
+++ abc098/d.py
@@ -1,20 +1,37 @@
-
 def m():
     n = int(input())
     a = list(map(int, input().split()))
-    a.insert(0 ,0)
+    a.insert(0, 0)
     left = 0
-    right = n-1
+    right = n
     ans = 0
-    for l in range(1, n+1):
-        for r in range(n, l-1, -1):
-            tmp = a[l]
-            for i in a[l+1:r+1]:
-                tmp = tmp ^ i
-            s = sum(a[l:r+1])
-            if tmp == s:
-                ans += 1
-                ans += abs(l-r)
-                break
+    for l in range(1, n + 1):
+        tmp = a[l]
+        for i in a[l + 1:right + 1]:
+            tmp = tmp ^ i
+        s = sum(a[l:right + 1])
+        if tmp == s:
+            ans += 1
+            ans += abs(l - right)
     return ans
-print(m())
+
+
+def someone_ans():
+    n = int(input())
+    a = list(map(int, input().split()))
+    ans = 0
+    r = 0
+    xor_tmp = 0
+    sum_tmp = 0
+    for l in range(n):
+        while r < n and (xor_tmp ^ a[r] == sum_tmp + a[r]):
+            xor_tmp = xor_tmp ^ a[r]
+            sum_tmp = sum_tmp + a[r]
+            r += 1
+        xor_tmp = xor_tmp ^ a[l]
+        sum_tmp = sum_tmp - a[l]
+        ans += r - l
+    return ans
+
+# print(m())
+print(someone_ans())
