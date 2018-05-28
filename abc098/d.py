66f8f30f0f60f79fc335d1a8610c3df21ad0c725
hayashi <sshayashi0208@gmail.com>
2018-05-28 21:37:50 +0900

    😭TLE

--- abc098/d.py
+++ abc098/d.py
@@ -2,11 +2,19 @@
 def m():
     n = int(input())
     a = list(map(int, input().split()))
-    for l in range(n):
-        for r in range(l,n):
-            pass
-        pass
-
-    pass
-
+    a.insert(0 ,0)
+    left = 0
+    right = n-1
+    ans = 0
+    for l in range(1, n+1):
+        for r in range(n, l-1, -1):
+            tmp = a[l]
+            for i in a[l+1:r+1]:
+                tmp = tmp ^ i
+            s = sum(a[l:r+1])
+            if tmp == s:
+                ans += 1
+                ans += abs(l-r)
+                break
+    return ans
 print(m())
