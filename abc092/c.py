15ba7dd757f0c0c0754b74c3b4c34b9305151a06
hayashi <sshayashi0208@gmail.com>
2018-05-30 14:42:02 +0900

    😷AC💯

--- abc092/c.py
+++ abc092/c.py
@@ -4,8 +4,14 @@ def m():
     total = abs(a[0]) + abs(a[n-1])
     for i in range(1, n):
         total += abs(a[i] - a[i-1])
+    for j in range(n):
+        if j == 0:
+            print(total - abs(a[j]) - abs(a[j]-a[j+1]) + abs(a[j+1]))
+            continue
+        if j == n-1:
+            print(total - abs(a[j-1]-a[j]) - abs(a[j]) + abs(a[j-1]))
+            continue
+        print(total - abs(a[j-1]-a[j]) - abs(a[j]-a[j+1]) + abs(a[j+1]-a[j-1]))
 
-    return total
 
-
-print(m())
+m()
