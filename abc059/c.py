a3de0d84471d25cc853e46a199747819171795fc
hayashi <sshayashi0208@gmail.com>
2018-06-08 18:16:40 +0900

    😭Give up

--- abc059/c.py
+++ abc059/c.py
@@ -1,37 +1,19 @@
 def m():
     n = int(input())
     a = list(map(int, input().split()))
-    even = a[1::2]
-    odd = a[::2]
-    ever_total = 0
-    ans = 0
-    if sum(even) > sum(odd):
-        # マイナススタート
-        for i in range(n):
-            ever_total += a[i]
-            if (i+2) % 2 == 0:
-                if ever_total <= 0:
-                    pass
-                else:
-                    ans += abs(a[i-1]) + 1
+    total = [0, 0]
+    cnt = [0, 0]
+    for i in range(2):
+        for j in range(n):
+            total[i] += a[j]
+            if (i+j)%2 == 0:
+                if total[i] >= 0:
+                    cnt[i] += abs(total[i]+1)
+                    total[i] = -1
             else:
-                if ever_total >= 0:
-                    pass
-                else:
-                    ans += abs(a[i - 1]) + 1
-    else:
-        # プラス
-        for i in range(n):
-            ever_total += a[i]
-            if (i+2) % 2 == 0:
-                if ever_total >= 0:
-                    pass
-                else:
-                    ans += abs(a[i-1]) + 1
-            else:
-                if ever_total <= 0:
-                    pass
-                else:
-                    ans += abs(a[i - 1]) + 1
-    return ans
+                if total[i] <= 0:
+                    cnt[i] += abs(total[i]-1)
+                    total[i] = 1
+
+    return min(cnt)
 print(m())
