87279e5188d3304c4a94b1a008ba349b83dac7cd
hayashi <sshayashi0208@gmail.com>
2018-06-11 10:04:06 +0900

    😷AC

--- abc057/c.py
+++ abc057/c.py
@@ -1,7 +1,14 @@
 def m():
     n = int(input())
-
-    pass
-
+    i=1
+    div = []
+    while i*i <= n:
+        if n % i == 0:
+            div.append(i)
+        i+=1
+    a = max(div)
+    b = n // a
+    d = max(a,b)
+    return len(str(d))
 
 print(m())
