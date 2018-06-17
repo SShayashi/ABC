acf480498a91e2c5cdd1ff53002fb403c5090258
hayashi <sshayashi0208@gmail.com>
2018-06-17 23:35:04 +0900

    😘AC

--- abc100/d.py
+++ abc100/d.py
@@ -1,17 +1,25 @@
-def m():
+def main():
     n, m = map(int, input().split())
     a = []
+    f = [-1, 1]
     for _ in range(n):
         x, y, z = map(int, input().split())
-        a.append((x, y, z))
-    xsum = 0
-    ysum = 0
-    zsum = 0
-    for i in range(n):
-        xsum += a[i][0]
-        ysum += a[i][1]
-        zsum += a[i][2]
+        a.append((x,y,z))
+    ans = 0
+    for i in f:
+        for k in f:
+            for p in f:
+                a.sort(key=lambda t: t[0] * i +t[1] * k +t[2] * p)
+                sx = 0
+                sy = 0
+                sz = 0
+                for j in range(m):
+                    sx += a[j][0]
+                    sy += a[j][1]
+                    sz += a[j][2]
+                s = abs(sx) + abs(sy) + abs(sz)
+                ans = max(ans, s)
+    return ans
 
-    pass
 
-print(m())
+print(main())
