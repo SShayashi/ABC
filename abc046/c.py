c5e90fde6bca2859d9551457df540aec9159d61d
hayashi <sshayashi0208@gmail.com>
2018-06-20 01:03:24 +0900

    😭WA

--- abc046/c.py
+++ abc046/c.py
@@ -1,19 +1,50 @@
 def m():
     n = int(input())
-    tt, aa = map(int, input().split())
+    nowt, nowa = map(int, input().split())
     for _ in range(n-1):
         t, a = map(int, input().split())
         if t == a:
-            tt = max(tt, aa)
-            aa = max(tt, aa)
+            nowt = max(nowt, nowa)
+            nowa = max(nowt, nowa)
         elif t > a:
-            if aa % a == 0:
-                pass
-            else:
+            if nowa < a:
+                nowa = a
+                nowt = int(nowa * (t / a))
+                continue
 
-            tt = int(aa * (t / a))
+            if nowt > nowa:
+                if nowt < t:
+                    nowt = t
+                    nowa = int(nowt * (a / t))
+                    continue
+
+                nowa = int((a / t) * nowt)
+                continue
+
+            for i in range(nowa):
+                if nowa < i * a:
+                    nowa = i*a
+                    break
+            nowt = int(nowa * (t / a))
         else:
-            aa = int(tt * (a / t))
-    return aa + tt
+            if nowt < t:
+                nowt = t
+                nowa = int(nowt * (a / t))
+                continue
+
+            if nowa > nowt:
+                if nowa < a:
+                    nowa = a
+                    nowt = int(nowa * (t / a))
+                    continue
+                nowt = int((t / a) * nowa)
+                continue
+
+            for j in range(nowt):
+                if nowt < j * t:
+                    nowt = j * t
+                    break
+            nowa = int(nowt * (a / t))
+    return nowa + nowt
 
 print(m())
