46d0758a7bbde9ca03717909f4fd84d49cb38412
hayashi <sshayashi0208@gmail.com>
2018-06-28 22:50:44 +0900

    WIP

--- abc044/c.py
+++ abc044/c.py
@@ -3,8 +3,18 @@ def m():
     x = list(map(int, input().split()))
     mm = max(x)
     dp = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(n*mm)]
-    for i in range(n):
-        for j in range(n):
+    dp[0][0][0] = 1
+    for j in range(n):
+        for k in range(n):
+            for s in range(n*mm):
+                if j == 0 and k == 0 and s == 0:
+                    continue
+                if j >= 1 and k >= 1 and s >= x[j]:
+                    dp[j][k][s] = dp[j-1][k][s] + dp[]
+                if j >= 1 and s < x[j]:
+                    dp[j][k][s] = dp[j-1][j][s]
+
+                pass
             pass
     pass
 
