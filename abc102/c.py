62e799cdb7fc4ef8ee3871e659615bfb5b25e053
hayashi <sshayashi0208@gmail.com>
2018-07-01 22:29:57 +0900

    😪AC

--- abc102/c.py
+++ abc102/c.py
@@ -1,13 +1,12 @@
 def m():
     n = int(input())
     a = list(map(int, input().split()))
-    cnt = 0
     ans = 0
-    for i in range(1, n+1):
-        cnt += a[i-1]-i
-    b = cnt // n
-    for j in range(1, n+1):
-        ans += abs(a[j-1]-(b+j))
+    B = [a[i-1]-i for i in range(1, n+1)]
+    B.sort()
+    b = B[n//2]
+    for i in range(n):
+        ans += abs(B[i]-b)
     return ans
 
 print(m())
