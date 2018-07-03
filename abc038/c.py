15a58301d32ac65554dca30fd7214521525cff81
hayashi <sshayashi0208@gmail.com>
2018-07-03 23:37:09 +0900

    😆AC💯

--- abc038/c.py
+++ abc038/c.py
@@ -1,6 +1,28 @@
 def m():
+    n = int(input())
+    a = list(map(int, input().split()))
+    left = 0
+    right = 1
+    ans = 0
+    while right < n:
+        if a[right-1] < a[right]:
+            right+=1
+            continue
 
-    pass
+        if left-right == 1:
+            ans += 1
+            continue
+        d = right - left
+        ans += d * (d+1) // 2
+        left = right
+        right += 1
 
+    if left - right == 1:
+        ans += 1
+        return ans
+
+    d = right - left
+    ans += d * (d + 1) // 2
+    return ans
 
 print(m())
