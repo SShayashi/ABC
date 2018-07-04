2f0b9f3f1c79c906a098f216ef15f0e790d52f25
hayashi <sshayashi0208@gmail.com>
2018-07-04 11:29:33 +0900

    😆AC💯

--- abc037/c.py
+++ abc037/c.py
@@ -1,12 +1,13 @@
 def m():
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
-    b = [0] * n
     ans = 0
-    for i in range(n-k+1):
-        for j in range(k):
-            b[i+j] +=1
+    times = 1
     for i in range(n):
-        ans += a[i]*b[i]
+        ans += a[i] * times
+        if i < k-1:
+            times += 1
+        if i >= n-k:
+            times -= 1
     return ans
 print(m())
