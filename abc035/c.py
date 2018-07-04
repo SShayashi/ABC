970cb2875aec60b9e9f10b5c6f55c9407200dd86
hayashi <sshayashi0208@gmail.com>
2018-07-04 18:35:16 +0900

    😊AC

--- abc035/c.py
+++ abc035/c.py
@@ -11,4 +11,21 @@ def m():
     return s[2:]
 
 
-print(m())
+def ans():
+    n, q = map(int, input().split())
+    a = [0] * (n+2)
+    for _ in range(q):
+        l, r = map(int, input().split())
+        a[l] += 1
+        a[r+1] -= 1
+    answer = ''
+    for i in range(1, n+1):
+        a[i] += a[i-1]
+        if a[i] % 2 == 0:
+            answer += "0"
+        else:
+            answer += "1"
+    return answer
+
+
+print(ans())
