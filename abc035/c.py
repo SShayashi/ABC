1e64d7b713067183318b0cef81f4d828e742d77f
hayashi <sshayashi0208@gmail.com>
2018-07-04 17:37:04 +0900

    😭TLE

--- abc035/c.py
+++ abc035/c.py
@@ -1,11 +1,14 @@
 def m():
     n, q = map(int, input().split())
-    ans = '0b' + ('0' * n)
+    ans = 0
     for _ in range(q):
         l, r = map(int, input().split())
-        tmp = '0b' + ('0' * (l-1)) + ('1' * (r-l+1)) + ('0' * (n-r))
-        ans = bin(eval(ans) ^ eval(tmp))
-    if len(ans[2:]) < n:
-        return '0' * (n - len(ans[2:])) + ans[2:]
-    return ans[2:]
+        tmp = ((1 << r - l + 1) - 1) * (1 << (n - r))
+        ans = ans ^ tmp
+    s = bin(ans)
+    if len(s[2:]) < n:
+        return '0' * (n - len(s[2:])) + s[2:]
+    return s[2:]
+
+
 print(m())
