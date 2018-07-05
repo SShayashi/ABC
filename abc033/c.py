432433614e93d1a2e3d6c8d6680a4ec31c101832
hayashi <sshayashi0208@gmail.com>
2018-07-05 21:51:18 +0900

    😊AC

--- abc033/c.py
+++ abc033/c.py
@@ -1,39 +1,11 @@
 def m():
     s = str(input())
-    if len(s) == 1:
-        if s == "0":
-            return 0
-        else:
-            return 1
-
     ans = 0
-    nums = s[::2]
-    sign = s[1::2]
-    zero = 0
-    for i in range(len(nums)-1):
-        if sign[i] == "*":
-            if nums[i] == 0:
-                zero = 1
+    a = s.split("+")
+    for i in range(len(a)):
+        if "0" in a[i]:
             continue
-        if zero:
-            zero = 0
-            continue
-        if sign[i] == "+" and nums[i] != "0":
+        else:
             ans += 1
-
-    if sign[-1] == "+":
-        if nums[-1] == "0":
-            return ans
-        else:
-            return ans +1
-
-    if zero:
-        return ans
-    else:
-        if nums[-1] == "0":
-            return ans
-        else:
-            return ans+1
-
-
+    return ans
 print(m())
