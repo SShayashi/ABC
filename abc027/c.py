c073886b3a744354a135d5e49cba994f71956ffa
hayashi <sshayashi0208@gmail.com>
2018-07-09 22:54:54 +0900

    😭Give up

--- abc027/c.py
+++ abc027/c.py
@@ -1,8 +1,21 @@
-def m():
+def main():
     n = int(input())
-    # let's simulate!!
+    # 2で何回割れるか
+    i = 0
+    m = n
+    while m > 1:
+        m //= 2
+        i += 1
+    # 2で奇数回割れるなら tはx2，aはx2+1
+    if i %2 == 1:
+        r = [0,1]
+    else:
+        r = [1,0]
+    a,j = 1,0
+    while a <= n:
+        a = a*2 + r[j]
+        j ^=1
+    ans = ["Takahashi", "Aoki"]
+    return ans[j]
 
-    pass
-
-
-print(m())
+print(main())
