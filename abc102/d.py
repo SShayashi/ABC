ce7588d38bd1f9ad48871a9e57c91e6005c70426
hayashi <sshayashi0208@gmail.com>
2018-07-11 16:32:16 +0900

    😭Give up

--- abc102/d.py
+++ abc102/d.py
@@ -7,36 +7,19 @@ def m():
     ans = MAX
     left_i = 0
     right_i = 2
-    for i in range(1,n-2):
-        p,q = MAX,MAX
-        r,s = MAX,MAX
-        left_diff = MAX
-        right_diff = MAX
-        while left_i < i:
-            d = abs(a[left_i] - (a[i]-a[left_i]))
-            if d < left_diff:
-                left_diff = d
-                p = a[left_i]
-                q = a[i]-a[left_i]
-            else:
-                break
+    for mid in range(1,n-2):
+        while left_i < mid and abs(a[left_i+1] - (a[mid]-a[left_i+1])) < abs(a[left_i] - (a[mid]-a[left_i])):
             left_i += 1
-        left_i -=1
 
-        if right_i <= i:
-            right_i = i+1
+        if right_i <= mid:
+            right_i = mid+1
 
-        while right_i < n:
-            d = abs((a[right_i] - a[i]) - (a[n-1] - a[right_i]))
-            if d < right_diff:
-                right_diff = d
-                r = a[right_i] - a[i]
-                s = a[n-1] - a[right_i]
-            else:
-                break
+        while right_i < n-1 and abs((a[right_i+1] - a[mid]) - (a[n-1] - a[right_i+1])) < abs((a[right_i] - a[mid]) - (a[n-1] - a[right_i])):
             right_i += 1
-        right_i -= 1
-
+        p = a[left_i]
+        q = a[mid] - a[left_i]
+        r = a[right_i] - a[mid]
+        s = a[n-1] - a[right_i]
         ans = min(ans, max(p,q,r,s) - min(p,q,r,s))
     return ans
 
