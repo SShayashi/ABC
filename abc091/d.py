fdb31190bd11564719a7eb2339a693ef0fec3df0
hayashi <sshayashi0208@gmail.com>
2018-07-25 13:39:38 +0900

    😭Give up

--- abc091/d.py
+++ abc091/d.py
@@ -5,17 +5,26 @@ def m():
     n = int(input())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    b.sort()
     ans = 0
     for bit in range(MAXBIT):
-        t = 2 ** (bit + 1)
+        t = 2 ** bit
         cnt = 0
+        A = [0] * n
+        B = [0] * n
         for i in range(n):
-            tmp = t-a[i]
-            left = bisect.bisect_left(b, t-a[i])
-            right = bisect.bisect_right(b, t-a[i])
-            cnt += len(b[left:right])
-        ans += (cnt % 2) * (2 ** bit)
+            A[i] = a[i] % (t * 2)
+            B[i] = b[i] % (t * 2)
+        B.sort()
+        for i in range(n):
+            left = bisect.bisect_left(B, t*1-A[i])
+            right = bisect.bisect_right(B, (t*2-A[i])-1)
+            cnt += right-left
+
+            left = bisect.bisect_left(B, t*3-A[i])
+            right = bisect.bisect_right(B, (t*4-A[i])-1)
+            cnt += right-left
+
+        ans += (cnt % 2) * t
     return ans
 
 print(m())
