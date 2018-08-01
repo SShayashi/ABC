f04025b57d893c41ae293650f62a82ca79df775f
hayashi <sshayashi0208@gmail.com>
2018-08-01 23:30:37 +0900

    😭Give up

--- abc086/d.py
+++ abc086/d.py
@@ -1,10 +1,13 @@
 def m():
     n, k = map(int, input().split())
-    grid = [[0]*(k*2) for _ in range(k*2)]
+    grid = [[0 for _ in range(k*2)] for _ in range(k*2)]
     for _ in range(n):
         x, y, c = map(str, input().split())
         x, y = int(x), int(y)
-        grid[x % (k * 2)][((y+k) % (k * 2))] +=1
+        if c == "W":
+            grid[x % (k * 2)][(y+k) % (k * 2)] +=1
+        else:
+            grid[x % (k * 2)][y% (k * 2)] += 1
         # grid.append((x % (k * 2), (y+k) % (k * 2)))
 
     accum = [[0] * (k * 2) for _ in range(k * 2)]
@@ -22,22 +25,28 @@ def m():
             accum[i][j] += grid[i][j] + accum[i][j-1] + accum[i-1][j] - accum[i-1][j-1]
 
     def query(sx, sy, gx, gy):
+        if gx == -1 or gy == -1:
+            return 0
+        if sx == -1 and sy == -1:
+            return accum[gx][gy]
+        if sx == -1:
+            return accum[gx][gy] - accum[gx][sy]
+        if sy == -1:
+            return accum[gx][gy] - accum[sx][gy]
         return accum[gx][gy] - accum[sx][gy] - accum[gx][sy] + accum[sx][sy]
 
     ans = 0
-    for i in range(k*2):
-        for j in range(k*2):
+    MAX = k*2 -1
+    MIN = -1
+    for i in range(-1, MAX, 1):
+        for j in range(-1, MAX, 1):
             cnt = 0
-            if i <= k and j <= k:
-                pass
-            if i == 0 and j == 0:
-                pass
-            if i == 0:
-                pass
-            if j == 0:
-                pass
+            cnt += query(i, j, min(i + k, MAX), min(j + k, MAX))
+            cnt += query(min(i + k, MAX), max(j - k, MIN), min(i + k, MAX), min(j, MAX))
+            cnt += query(min(i + k, MAX), min(j + k, MAX), min(i + k * 2, MAX), min(j + k * 2, MAX))
+            cnt += query(max(i - k, MIN), min(j+k, MAX), i, min(j + k * 2, MAX))
+            cnt += query(max(i-k, MIN), max(j-k, MIN), i, j)
             ans = max(cnt, ans)
-    pass
-
+    return ans
 
 print(m())
