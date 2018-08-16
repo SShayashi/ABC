d3c66bdb8da68e7b3e989242ef2b9daa7401957c
hayashi <sshayashi0208@gmail.com>
2018-08-16 19:20:54 +0900

    😌AC

--- abc104/b.py
+++ abc104/b.py
@@ -2,7 +2,7 @@ def m():
     s = str(input())
     if s[0] != "A":
         return "WA"
-    if s[2:len(s)-3].count("C") != 1:
+    if s[2:len(s)-1].count("C") != 1:
         return "WA"
     for c in s[1:]:
         if c == "C":
