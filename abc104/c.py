00983cc3c0b407fe5e5018fdb0c1d67727648ef9
hayashi <sshayashi0208@gmail.com>
2018-08-16 21:51:37 +0900

    ☺AC

--- abc104/c.py
+++ abc104/c.py
@@ -26,4 +26,42 @@ def m():
                 b.remove(b[k])
                 break
 
-print(m())
+def after_explain():
+    d, g = map(int, input().split())
+    a = []
+    b = []
+    for i in range(d):
+        p, c = map(int, input().split())
+        a.append(p)
+        b.append(c)
+
+    mincnt = 999999999
+    for i in range(1 << 2 * d):
+        score = 0
+        cnt = 0
+        for j in range(d):
+            if i & (1 << j):
+                score += a[j]*(j+1)*100 + b[j]
+                cnt += a[j]
+
+        for j in range(d-1, -1, -1):
+            if not(i & (1 << j)):
+                if score > g:
+                    break
+                s = g - score
+                if s % ((j+1)*100) == 0:
+                    c = min(a[j], s // ((j + 1) * 100))
+                    cnt += c
+                    score += c * ((j + 1) * 100)
+                else:
+                    c = min(a[j], s // ((j + 1) * 100))
+                    cnt += c + 1
+                    score += (c+1) * ((j + 1) * 100)
+                break
+        if score < g:
+            continue
+        if cnt < mincnt:
+            mincnt = cnt
+    return mincnt
+
+print(after_explain())
