5d8cdfb499f9a10758715175320d0f21e5015b28
hayashi <sshayashi0208@gmail.com>
2018-10-11 21:35:55 +0900

    WIP

---
+++ abc104/d.py
@@ -0,0 +1,13 @@
+MOD = 10 ** 9 + 7
+
+
+def m():
+    s = str(input())
+    l = len(s)
+    dp = [[0 for _ in range(4)] for _ in range(l+1)]
+    if l < 3:
+        return 0
+    for i in range(l, 0, -1):
+        for j in range(4, 0, -1):
+            pass
+    pass
