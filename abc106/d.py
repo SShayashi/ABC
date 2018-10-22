f5e9c9e409e38ce3387d7500042f87e4d15f1a21
hayashi <sshayashi0208@gmail.com>
2018-10-22 09:58:17 +0900

    😤AC

--- abc106/d.py
+++ abc106/d.py
@@ -1,20 +1,23 @@
 def m():
     n, M, Q = map(int, input().split())
-    L, R = 0, 1
-    a = [[0 for _ in range(n+1)] for _ in range(2)]
+    a = [[0 for _ in range(n+1)] for _ in range(n+1)]
+    b = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
     for i in range(M):
         l, r = map(int, input().split())
-        a[L][l] += 1
-        a[R][r] += 1
-    b = [[0 for _ in range(n+1)] for _ in range(2)]
+        a[l][r] += 1
 
-    for i in range(2):
+    for i in range(1, n+1):
+        b[1][i] += a[1][i] + b[1][i-1]
+    for i in range(2, n+1):
+        b[i][1] += a[i][1] + b[i-1][1]
+
+    for i in range(1, n+1):
         for j in range(1, n+1):
-            b[i][j] = a[i][j] + b[i][j-1]
+            b[i][j] = a[i][j] + b[i-1][j] + b[i][j-1] - b[i-1][j-1]
 
     for i in range(Q):
         p, q = map(int, input().split())
-        left = b[L][p-1]
-        right = b[R][q] - b[R][p]
-        print(right-left)
+        ans = b[q][q] + b[p-1][p-1] - b[q][p-1] - b[p-1][q]
+        print(ans)
+
 m()
