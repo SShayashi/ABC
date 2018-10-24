cb7157e359a7bddd981d2a9ec5cd142f27971265
hayashi <sshayashi0208@gmail.com>
2018-10-24 10:57:39 +0900

    😝AC

--- abc107/c.py
+++ abc107/c.py
@@ -4,23 +4,19 @@ import bisect
 def m():
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
-    dp = dict()
-    CENTER = bisect.bisect_right(a, 0)
-    maxixmum = a[n-1]
-    minimum = a[0]
-    left_side = CENTER
-    right_side = n - CENTER
-
-    if a[CENTER] == a[CENTER-1]:
-        pass
-    if a[CENTER] > a[CENTER-1]:
-        pass
-
-    for i in range(k):
-        a[CENTER]
-        pass
-
-    pass
-
+    b = [abs(i) for i in a]
+    right = bisect.bisect_right(a,0)
+    left = right-1
+    ans = 99999999999
+    for i in range(n-k+1):
+        s = 0
+        if left >= i+k-1:
+            s = b[i]
+        elif right <= i:
+            s = b[i+k-1]
+        else:
+            s = min(b[i] + b[i+k-1]*2, b[i]*2 + b[i+k-1])
+        ans = min(ans, s)
+    return ans
 
 print(m())
