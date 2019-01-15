e9687cd1785094ca4d0e9fef2ec6247037c03667
hayashi <sshayashi0208@gmail.com>
2019-01-16 08:38:47 +0900

    😭WA

---
+++ keyence2019/d.py
@@ -0,0 +1,14 @@
+MOD = 10 ** 9 + 7
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+def m():
+    d = [[-1 for _ in range(N)] for _ in range(M)]
+    for i in range(N):
+        for j in range(M):
+
+            pass
+    pass
+
+print(m())
