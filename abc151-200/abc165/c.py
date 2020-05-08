510861b769273daf81ed801714e70d723d5869ee
hayashi <sshayashi0208@gmail.com>
2020-05-08 22:58:14 +0900

    AC

--- abc151-200/abc165/c.py
+++ abc151-200/abc165/c.py
@@ -5,3 +5,41 @@
 # それらを配列とみなす．
 # それで全探索し，最も得点が高いものの組み合わせが答え
 # 19C9 は3万程度なのでOK
+
+import itertools
+
+
+def main():
+    n, m, q = map(int, input().split())
+
+    A, B, C, D = [], [], [], []
+    for _ in range(q):
+        a, b, c, d = map(int, input().split())
+        A.append(a)
+        B.append(b)
+        C.append(c)
+        D.append(d)
+    if m == 1:
+        return sum(D)
+    ans = 0
+    length = m+n-1
+    for item in itertools.combinations(range(length), m-1):
+        ptn = []
+        num = 1
+        p = 0
+        for i in range(length):
+            if i == item[p]:
+                num += 1
+                p += 1
+                p = min(m-2, p)
+                continue
+            ptn.append(num)
+        tmp = 0
+        for k in range(q):
+            if (ptn[B[k]-1] - ptn[A[k]-1]) == C[k]:
+                tmp += D[k]
+
+        ans = max(tmp, ans)
+    return ans
+
+print(main())
--- abc151-200/abc165/d.py
+++ abc151-200/abc165/d.py
@@ -4,10 +4,6 @@ def m():
     def calc(x):
         return int((a * x) / b) - (a * int(x / b))
 
-    if calc(1) < calc(n // 2):
-        return max([calc(i) for i in range(n // 2, n + 1)])
-    else:
-        return max([calc(i) for i in range(1, n // 2 + 1)])
-
+    return calc(min(n, b-1))
 
 print(m())
