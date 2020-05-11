b43539ef4f5b65ddf5cd0fdc37bdcd171b39c0cf
hayashi <sshayashi0208@gmail.com>
2020-05-11 21:04:57 +0900

    use itertools.product([True, False], repeat=N)

--- abc151-200/abc167/c.py
+++ abc151-200/abc167/c.py
@@ -1,29 +1,21 @@
+import itertools
+
+
 def m():
     N, M, X = map(int, input().split())
     CA = [list(map(int, input().split())) for _ in range(N)]
     ans = float('inf')
-    for i in range(2 ** N):
-        b = bin(i)[2:]
-        b = b.zfill(N)
+    for i in itertools.product([True, False], repeat=N):
         tmp = [0] * (M + 1)
-        for j, v in enumerate(b):
-            if v == '0':
-                continue
-            tmp[0] += CA[j][0]
-            for k in range(1, M + 1):
-                tmp[k] += CA[j][k]
+        for j, v in enumerate(i):
+            if v:
+                for k in range(M + 1):
+                    tmp[k] += CA[j][k]
         f = 1
         for p in range(1, M + 1):
-            if tmp[p] < X:
-                f = 0
-                break
-        if f == 0:
-            continue
-        ans = min(ans, tmp[0])
-    if ans == float('inf'):
-        return '-1'
-    else:
-        return ans
+            if tmp[p] < X: f = 0
+        if f: ans = min(ans, tmp[0])
+    return ans if ans != float('inf') else "-1"
 
 
 print(m())
