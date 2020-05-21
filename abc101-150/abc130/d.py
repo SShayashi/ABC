ff195b137c66de380c7eb12c1c38edd5b858518d
hayashi <sshayashi0208@gmail.com>
2020-05-22 00:49:05 +0900

    AC

--- abc101-150/abc130/d.py
+++ abc101-150/abc130/d.py
@@ -1,20 +1,23 @@
 def m():
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
-    acum = [0] * n
-    acum[0] = a[0]
-    for i in range(1, n):
-        acum[i] = a[i] + acum[i - 1]
+    acum = [0] * (n+1)
+    acum[0] = 0
+    for i in range(n):
+        acum[i+1] = a[i] + acum[i]
     left = right = ans = 0
-    while left < n:
-        diff =(acum[right] - acum[left])
+    while left <= n:
+        diff = acum[right] - acum[left]
         if diff >= k:
-            ans += (n-right)
-            left+=1
-            right = left
+            ans += (n - right)+1
+            left += 1
+
         elif diff < k:
-            right+=1
+            right += 1
+            if right > n:
+                return ans
+
     return ans
 
-print(m())
 
+print(m())
