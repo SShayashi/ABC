a873dff5a51b14d0774810fe22a005918e696b37
hayashi <sshayashi0208@gmail.com>
2020-05-30 23:49:42 +0900

    AC

--- abc101-150/abc141/d.py
+++ abc101-150/abc141/d.py
@@ -1,9 +1,20 @@
+import queue
+
+
 def m():
-    N,M = map(int, input().split())
+    N, M = map(int, input().split())
+    q = queue.PriorityQueue()
     A = list(map(int, input().split()))
-    import heapq
-    heapq.heapify(A)
-    pass
+    ans = []
+    for a in A:
+        q.put(-a)
+    while M > 0:
+        x = -q.get()
+        x = -(x >> 1)
+        M -= 1
+        q.put(x)
+    while not q.empty(): ans.append(q.get())
+    return abs(sum(ans))
 
 
 print(m())
