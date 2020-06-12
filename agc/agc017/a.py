4f21bf4cb83ccfe5a43f77e96abb37981d98944a
hayashi <sshayashi0208@gmail.com>
2020-06-12 21:59:42 +0900

    解説AC

--- agc/agc017/a.py
+++ agc/agc017/a.py
@@ -1,22 +1,32 @@
+"""
+奇数が一つでもあるとする．
+
+それ以外の2**N-1 の組み合わせと，その奇数を選ぶか否かで
+結果が奇数になるか偶数になるか操作できる．
+
+P=0の，偶数の数を求めたい場合を考える．
+2**N-1の数の組み合わせの中の一つに奇数があれば，ある奇数を選ぶ．
+そうでない場合は，余っている奇数を選ばない．
+というふうにすれば2**N-1の組み合わせすべてを偶数の数にすることができる．
+よって答えは 2**N-1.
+
+同様に奇数の場合も，ある奇数を選ぶか選ばないかで，選んだものの合計を奇数に固定できる．
+よって，2**N-1
+
+"""
+
 def m():
     N, P = map(int, input().split())
     A = list(map(int, input().split()))
     Even = [a for a in A if a % 2 == 0]
     Odd = [a for a in A if a % 2 == 1]
-    import itertools
-    if P == 0:
-        even_cnt = len(Even) * (len(Even)+1) // 2
-        odd_cnt = 0
-        for i in range(1, len(Odd)+1, 2):
-            odd_cnt += len(list(itertools.combinations(Odd, i)))
-        return max(even_cnt,1) * max(1, odd_cnt)
+    if P == 1 and len(Odd) == 0:
+        return 0
+
+    if len(Odd) == 0:
+        return pow(2, len(Even))
     else:
-        even_cnt = odd_cnt = 0
-        for i in range(1, len(Odd)+1, 2):
-            odd_cnt += len(list(itertools.combinations(Odd, i)))
-        for i in range(0, len(Even)+1, 2):
-            even_cnt += len(list(itertools.combinations(Even, i)))
-        return max(1,even_cnt)*max(1,odd_cnt)
+        return pow(2, N-1)
 
 
 print(m())
