3c57af39e4980056307bdc388f284e328ff51e95
hayashi <sshayashi0208@gmail.com>
2020-06-19 23:49:51 +0900

    解説AC

--- arc/arc037/b.py
+++ arc/arc037/b.py
@@ -2,40 +2,33 @@ import sys
 
 sys.setrecursionlimit(10**7)
 
-def test():
-    N, M = 100, 100*99//2
-    G = [[] for _ in range(N + 1)]
-    for i in range(1, 101):
-        for j in range(1, 101):
-
-            u, v = i, i+1
-            G[u].append(v)
-            G[v].append(u)
-    return N, M, G
-
+flag = True
 def m():
-    # N, M = map(int, input().split())
-    # G = [[] for _ in range(N + 1)]
-    # for _ in range(M):
-    #     u, v = map(int, input().split())
-    #     G[u].append(v)
-    #     G[v].append(u)
-    N, M = 100, 100
+    N, M = map(int, input().split())
     G = [[] for _ in range(N + 1)]
-    def dfs(node, me, parent, is_tree):
-        for n in node:
-            if n == parent: continue
-            if visited[n]: return False
-            if not dfs(G[n], n, me, is_tree): return False
-        return True
-
-    ans = 0
+    for _ in range(M):
+        u, v = map(int, input().split())
+        G[u].append(v)
+        G[v].append(u)
     visited = [False] * (N + 1)
+
+    def dfs(me, parent):
+        global flag
+        for n in G[me]:
+            if n == parent: continue
+            if visited[n]:
+                flag = False
+            else:
+                visited[n] = True
+                dfs(n, me)
+    ans = 0
     for i in range(1, N + 1):
+        global flag
+        flag = True
         if visited[i]:
             continue
-        visited[i] = True
-        if dfs(G[i], i, None, True): ans += 1
+        dfs(i, None)
+        if flag: ans += 1
     return ans
 
 
